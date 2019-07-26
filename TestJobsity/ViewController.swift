b38c5f33c00018e80bdb435f151fcdb1efed61bf
Daniel Klinkert <daniel.klinkert@gmail.com>
2019-07-26 10:55:56 -0500

    Initial Commit

---
+++ TestJobsity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestJobsity
+//
+//  Created by Daniel Klinkert on 7/26/19.
+//  Copyright © 2019 Jobsity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
